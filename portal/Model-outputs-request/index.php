e773a5448a308b680308a5b9e261f896b680a722
esteban-em <mercier.esteban@gmail.com>
2018-07-02 16:31:55 +0200

    coquille

--- portal/Model-outputs-request/index.php
+++ portal/Model-outputs-request/index.php
@@ -3,8 +3,8 @@ if (!isset($_SESSION)) {
   session_start();
 }
 
-require_once 'conf/define-project.php'; ;
-$project_url = "/Cerdanya";
+require_once 'conf/define-project.php';
+$project_url = "/";
 require_once 'conf/conf.php';
 $titreMilieu = "Model outputs request";
 ob_start();
